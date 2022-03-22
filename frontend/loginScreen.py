3f23a85a155e1593f30c60bf4490d94c465ae1c2
unknown <patryk2710dd@gmail.com>
2022-03-22 21:06:16 +0000

    skeleton input screen done

---
+++ frontend/loginScreen.py
@@ -0,0 +1,7 @@
+import tkinter
+from tkinter import ttk
+
+
+def layout(inputFrame):
+    label = tkinter.Label(inputFrame, text="This is the Login Screen")
+    label.pack()
--- frontend/main.py
+++ frontend/main.py
@@ -9,7 +9,12 @@ from matplotlib import pyplot as plt
 from matplotlib import image as image
 
 
+# this draws the initial screen for beginning interaction
 def firstScreen():
+    global startButtonImage
+    startButtonImage = Image.open('assets/start.png')
+    startButtonImage = ImageTk.PhotoImage(startButtonImage)
+
     startButton = tkinter.Button(startFrame, image= startButtonImage, text="???", command=inputs)
     startButton.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
 
@@ -19,35 +24,26 @@ def firstScreen():
     startFrame.pack()
 
 
+# send user to screen for inputting bottles and cans
 def inputs():
     startFrame.pack_forget()
-    inputScreen.layout(inputFrame)
+    inputScreen.layout(inputFrame, root)
     inputFrame.pack()
 
 
-def readImages():
-    global startButtonImage
-    global bottleImage
-    global canImage
-    global finishImage
-
-    startButtonImage = Image.open('assets/start.png')
-    startButtonImage = ImageTk.PhotoImage(startButtonImage)
-
-
+# main starter code
 if __name__ == '__main__':
     root = tkinter.Tk()
-    readImages()
 
     s = ttk.Style()
     s.theme_use('winnative')
 
     startFrame = tkinter.Frame(root, height=900, width=1600, background='white')
-    inputFrame = tkinter.Frame(root)
+    inputFrame = tkinter.Frame(root, height=900, width=1600, background='white')
 
     firstScreen()
 
     root.wm_geometry("1600x900")
     root.configure(background="white")
-    root.title("Gaming???")
+    root.title("Reverse Vending Machine")
     root.mainloop()
