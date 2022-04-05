a9022b538da94b7f5bbb715d5b14edb0a833ee0b
unknown <patryk2710dd@gmail.com>
2022-04-05 19:46:19 +0100

    added looping to scripts

--- ssd.py
+++ ssd.py
@@ -1,5 +1,4 @@
 import numpy as np
-import tensorflow as tf
 import cv2
 import os
 import time
@@ -9,55 +8,78 @@ net = cv2.dnn.readNetFromTensorflow('frozen_inference_graph.pb', 'graph.pbtxt')
 # read in whole directory
 images = []
 imageName = []
-results = []
 for file in os.listdir('assets/'):
     image = cv2.imread(os.path.join('assets/', file))
     if image is not None:
         images.append(image)
         imageName.append(file)
 
-# kernel = np.ones((5,5),np.float32)/25
+kernel = np.ones((5,5),np.float32)/25
 
-i = 0
-startTime = time.time()
-for image in images:
-    found = False
-    CClockwiseImage = cv2.rotate(image, cv2.ROTATE_90_COUNTERCLOCKWISE)
+timeTaken = []
+results = []
+totalTime = time.time()
+for x in range(0,10):
+    i = 0
+    resultsCurr = []
+    startTime = time.time()
+    for image in images:
+        found = False
+        # rotating counter-clockwise increases accuracy
+        CClockwiseImage = cv2.rotate(image, cv2.ROTATE_90_COUNTERCLOCKWISE)
 
-    # adding filtering increases accuracy, (gaussian blur goes up to 74, averaging goes up to 76, median to 77)
-    # filtered = cv2.GaussianBlur(CClockwiseImage, (5, 5), 0) # gaussian
-    filtered = cv2.medianBlur(CClockwiseImage, 5)  # median
-    # filtered = cv2.filter2D(src=CClockwiseImage, ddepth=-1, kernel=kernel) # averaging
+        # adding filtering increases accuracy
+        # filtered = cv2.GaussianBlur(CClockwiseImage, (5, 5), 0) # gaussian
+        # filtered = cv2.medianBlur(CClockwiseImage, 5)  # median
+        # filtered = cv2.filter2D(src=CClockwiseImage, ddepth=-1, kernel=kernel) # averaging
 
-    blob = cv2.dnn.blobFromImage(filtered, size=(300, 300), swapRB=True, crop=False)
-    net.setInput(blob)
+        blob = cv2.dnn.blobFromImage(CClockwiseImage, size=(300, 300), swapRB=True, crop=False)
+        net.setInput(blob)
 
-    outputs = net.forward()
+        outputs = net.forward()
 
-    # index 1 is class number, index 2 is confidence score
-    for output in outputs[0, 0, :, :]:
-        score = float(output[2])
-        if score > 0.3:
-            if output[1] == 44:
-                found = True
+        # index 1 is class number, index 2 is confidence score
+        for output in outputs[0, 0, :, :]:
+            score = float(output[2])
+            if score > 0.25:
+                if output[1] == 44:
+                    found = True
 
-    if found:
-        if "fake" in imageName[i]:
-            results.append('Wrong')
+        if found:
+            if "fake" in imageName[i]:
+                resultsCurr.append('Wrong')
+            else:
+                resultsCurr.append('Correct')
         else:
-            results.append('Correct')
-    else:
-        if "fake" in imageName[i]:
-            results.append('Correct')
-            results.append('FCorrect')
-        else:
-            results.append('Wrong')
-    i += 1
-endTime = time.time()
+            if "fake" in imageName[i]:
+                resultsCurr.append('Correct')
+                resultsCurr.append('FCorrect')
+            else:
+                resultsCurr.append('Wrong')
+        i += 1
+        # clean and store
+    endTime = time.time()
+    timeTaken.append(endTime - startTime)
+    results.append(resultsCurr)
+totalTimeEnd = time.time()
 
 # here analyse results
-percentCorrect = (results.count('Correct')/len(images)) * 100
-print("Correct: " + str(results.count('Correct')) + " out of " + str(len(images)) +
-      " (" + "{:.3f}".format(percentCorrect) + "%)")
-print("Non bottle object correctly ignored: " + str(results.count('FCorrect')))
-print("Single Shot Detector took {:.6f} seconds to run ".format(endTime - startTime) + str(len(images)) + " images.")
+percentCorrect = []
+Correct = []
+FakeCorrect = []
+for x in results:
+    Correct.append(x.count('Correct'))
+    percentCorrect.append(x.count('Correct')/len(images) * 100)
+    FakeCorrect.append(x.count('FCorrect'))
+
+averageCorrect = np.mean(Correct)
+averageTime = np.mean(timeTaken)
+averagePercCorrect = np.mean(percentCorrect)
+averageFakeCorrect = np.mean(FakeCorrect)
+
+print("Single Shot Detector Completed 10 runs on " + str(len(images)) + " images in: " +
+      "{:.5f}".format(totalTimeEnd - totalTime) + "s")
+print("Averages per run:")
+print("     Run time: " + "{:.5f}".format(averageTime) + "s")
+print("     Accuracy: " + "{:.0f}".format(averageCorrect) + "/92 (" + "{:.3f}".format(averagePercCorrect) + "%)")
+print("     Non bottle corrrectly ignored: " + "{:.0f}".format(averageFakeCorrect) + "/10")
